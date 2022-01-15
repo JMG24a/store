27f2f523b2fcd2ceab6fa8134276c3c5f319c5b4
jmg24a <jmg24a@gmail.com>
2022-01-15 12:02:59 -0400

    actualizacion a las configuraciones del proyecto en package.json, webpack, y .babelrc

---
+++ webpack.config.dev.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    mode: 'development',
+    //watch: false,
+    resolve:{
+        extensions: ['.js','.jsx'],    
+        alias:{
+            '@logos': path.resolve(__dirname,'src/assets/logos/'),
+            '@icons': path.resolve(__dirname,'src/assets/icons/'),
+            '@containers': path.resolve(__dirname, 'src/containers/')
+        },
+    },
+
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use:{
+                    loader:'html-loader'
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|svg)$/,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new copyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src/assets/logos"),
+                    to: 'assets/logos'
+                }
+            ]
+        }),
+    ],
+    devServer:{
+        static: path.join(__dirname,'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+        open: false,
+    },
+}
+
